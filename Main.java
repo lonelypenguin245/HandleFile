78be74672e6fd52da337c2192d06c91a979a959c
lonelypenguin245 <144401532+lonelypenguin245@users.noreply.github.com>
2023-10-13 07:54:47 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package HandleFile;
+
+/**
+ *
+ * @author HI
+ */
+public class Main {
+    public static void main(String[] args) {
+        //loop until user want to exit
+        while (true) {
+            int choice = Manager.menu();
+            switch (choice) {
+                case 1:
+                    Manager.findPersonInfo();
+                    break;
+                case 2:
+                    Manager.coppyNewFile();
+                    break;
+                case 3:
+                    return;
+            }
+        }
+    }
+}
